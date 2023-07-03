2ffa8c296e8c66f413c841510c49e78313c43962
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-03 17:47:35 -0300

    feat:criando de contatos, adicionando icones

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,14 +1,29 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import Container from "../../components/Container"
+import styles from "./Contatos.module.css"
+import { GoMail } from "react-icons/go"
+import {BsInstagram, BsWhatsapp,BsGithub, BsLinkedin } from "react-icons/bs"
 
 function Contatos() {
     return (
         <>
             <Header />
                 <Container>
-                   
-                        <h2>Contatos</h2>
+                   <section className={styles.contatos}>
+                          <h2>Contatos</h2>
+                          <h3>Entre em contato</h3>
+                          <p>Para que possamos conversar mais sobre.</p>
+                          <div className={styles.icones}>
+                            <GoMail />
+                            <BsInstagram />
+                            <BsWhatsapp />
+                            <BsGithub />
+                            <BsLinkedin />
+
+                          </div>
+                   </section>
+                       
                     
                 </Container>
             <Footer />

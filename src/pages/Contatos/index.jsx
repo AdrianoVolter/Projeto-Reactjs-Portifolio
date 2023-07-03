769daaef291d518d3feabd943030ea22d5ce9905
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-03 18:07:58 -0300

    feat:adicionado links nos icones

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -15,11 +15,46 @@ function Contatos() {
                           <h3>Entre em contato</h3>
                           <p>Para que possamos conversar mais sobre.</p>
                           <div className={styles.icones}>
-                            <GoMail className={styles.icone}/>
+
+                            <a 
+                                href="mailto:adriano.j.volter@hotmail.com" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                >
+                                <GoMail className={styles.icone}/>
+                            </a>
+                           
+                            <a
+                                href="https://www.instagram.com/adriano.volter"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
                             <BsInstagram className={styles.icone}/>
+                            </a>
+
+                            <a
+                                href="https://api.whatsapp.com/send?phone=5548998524934"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
                             <BsWhatsapp className={styles.icone}/>
+                            </a>
+
+                            <a
+                                href="https://github.com/AdrianoVolter"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
                             <BsGithub className={styles.icone}/>
+                            </a>
+
+                            <a
+                                href="https://www.linkedin.com/in/adrianovolter/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
                             <BsLinkedin className={styles.icone}/>
+                            </a>
 
                           </div>
                    </section>

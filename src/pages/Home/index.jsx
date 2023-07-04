2ffd04833def6784214b7dd47ca00db93770c91f
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-04 09:53:49 -0300

    feat: Adicionando rotas aninhadas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,37 +1,31 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
 import styles from './Home.module.css'
 import { Link } from 'react-router-dom'
-
 
 function Home () {
   return (
     <>
-        <Header/>
-            <Container>
-            <section className={styles.home}>
+    
+        <section className={styles.home}>
             <div className={styles.apresentacao}>
-            <p>Olá,  sou <br />
-                <span>Adriano Volter</span> <br />
-                Dev Full Stack
-            </p>
-            <Link 
-                to = '/sobre'
-                className={`${styles.btn} ${styles.btn_blue }`}>
-                Saiba mais sobre mim
-            </Link>
+                <p>Olá,  sou <br />
+                    <span>Adriano Volter</span> <br />
+                    Dev Full Stack
+                </p>
+                <Link 
+                    to = '/sobre'
+                    className={`${styles.btn} ${styles.btn_blue }`}>
+                    Saiba mais sobre mim
+                </Link>
             </div>
             <figure>
-            <img 
-                className={styles.img_home}
-                src="public/undraw_programming_re_kg9v.svg" 
-                alt="Imagem Home" 
-                />
+                <img 
+                    className={styles.img_home}
+                    src="public/undraw_programming_re_kg9v.svg" 
+                    alt="Imagem Home" 
+                    />
             </figure>
         </section>
-            </Container>
-        <Footer/>
+         
     </>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,18 +4,24 @@ import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
 import Contatos from './pages/Contatos';
 import Page404 from './pages/Page404';
-
+import Header from './components/Header';
+import Container from './components/Container';
+import Footer from './components/Footer';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/projetos" element={<Projetos />} />
-            <Route path="/contatos" element={<Contatos />} /> 
-            <Route path="*" element={<Page404 />} />           
-        </Routes>
+        <Header />
+        <Container>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
+              <Route path="/projetos" element={<Projetos />} />
+              <Route path="/contatos" element={<Contatos />} /> 
+              <Route path="*" element={<Page404 />} />           
+          </Routes>
+        </Container>
+        <Footer />
     </BrowserRouter>
   );
 }

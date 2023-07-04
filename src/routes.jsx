1f01c4a99468b20e61ea6c7c199c5395e2da274f
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-04 10:31:03 -0300

    feat: criado pagina base , ajustando no arquivo routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,24 +4,20 @@ import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
 import Contatos from './pages/Contatos';
 import Page404 from './pages/Page404';
-import Header from './components/Header';
-import Container from './components/Container';
-import Footer from './components/Footer';
+import PageBase from './pages/PageBase';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-        <Header />
-        <Container>
           <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<Sobre />} />
-              <Route path="/projetos" element={<Projetos />} />
-              <Route path="/contatos" element={<Contatos />} /> 
-              <Route path="*" element={<Page404 />} />           
+                <Route path="/" element={<PageBase />}>
+                    <Route index element={<Home />} />
+                    <Route path="/sobre" element={<Sobre />} />
+                    <Route path="/projetos" element={<Projetos />} />
+                    <Route path="/contatos" element={<Contatos />} /> 
+                    <Route path="*" element={<Page404 />} /> 
+                </Route>       
           </Routes>
-        </Container>
-        <Footer />
     </BrowserRouter>
   );
 }

d0bd1ed37522c6ad8dccd148e3ea018df812816e
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-04 10:22:33 -0300

    feat: criado pagina base para rotas

---
+++ src/pages/PageBase/index.jsx
@@ -0,0 +1,19 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Container from '../../components/Container'
+import { Outlet } from 'react-router-dom'
+
+
+function PageBase () {
+  return (
+   <main>
+        <Header />
+        <Container>
+           <Outlet />
+        </Container>
+        <Footer />
+   </main>
+  )
+}
+
+export default PageBase

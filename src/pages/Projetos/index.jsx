4359a524b45d56417a83e424b3b1a25ee4344afc
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-05 14:31:27 -0300

    feat: Função buscar dados da api do github

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,7 +1,20 @@
 import Card from "../../components/Card"
 import  {projetos, lista}  from "./Card.module.css"
+import { useEffect, useState } from "react"
 
 function Projetos() {
+
+    const [ repositories, setRepositories ] = useState([]) 
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://api.github.com/users/adrianovolter/repos')
+            const data = await response.json()
+            setRepositories(data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <section className={projetos}>     
             <h2>Projetos</h2>

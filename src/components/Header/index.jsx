6dee129054470d66622d79c96d480541ac41cdd5
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-07 21:54:10 -0300

    feat: criado logica do menu sanwich

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false) 
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
+
+
+
     return (
         <header className={styles.header}>
             <Link to="/">
             <span>adrianoVolter.dev</span>
             </Link>
-            <nav className={styles.menu_sandwich}>
+            <nav className={`${styles.menu_sandwich} ${showMenu ? styles.show : ''}`}>
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
-            <div className={styles.menu_button}>
+            <div 
+                className={styles.menu_button}
+                onClick={toggleMenu}
+            >
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>

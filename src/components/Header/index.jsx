2803978b193f5c8af1e40f1d4417edf03733f9ee
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-07 21:41:25 -0300

    feat: criado o menu sanwich

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,12 +7,18 @@ function Header() {
             <Link to="/">
             <span>adrianoVolter.dev</span>
             </Link>
-            <nav>
+            <nav className={styles.menu_sandwich}>
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
+            <div className={styles.menu_button}>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
         </header>
     )
     }

4c5c95fe9c3d5f4f928d7d88cb92ad78227d4d54
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-28 13:22:09 -0300

    feat: Atualizando rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import{ BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
@@ -9,18 +9,19 @@ import PageBase from './pages/PageBase';
 function AppRoutes() {
   return (
     <BrowserRouter>
-          <Routes>
-                <Route path="/" element={<PageBase />}>
-                    <Route index element={<Home />} />
-                    <Route path="/sobre" element={<Sobre />} />
-                    <Route path="/projetos" element={<Projetos />} />
-                    <Route path="/contatos" element={<Contatos />} /> 
-                    <Route path="*" element={<Page404 />} /> 
-                </Route>       
-          </Routes>
+      <Routes>
+        <Route path="/" element={<PageBase />}>
+          <Route index element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/contatos" element={<Contatos />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default AppRoutes;
+
 

5c545a2a3adae7de8558958965e1b1784afa87c2
Adriano Jose Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-11-02 08:03:27 -0300

    Update index.jsx
    fix: mudança no texto sobre mim.

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -17,13 +17,13 @@ function Sobre() {
                 <img src={avatar} alt="" className={styles.avatar} />
                 <div className={styles.textos}>
                       <h2>Sobre</h2>
-                      <p>Olá, sou <span>Adriano Volter</span>, tenho 39 anos<br /> Sou estudante de <strong>Curso Dev FullStack</strong> no <strong>Senai/SC </strong> <br />Curso com 3 modulos, modulo 1 Front-End <br /> e modulo 2 Back-End completo.
+                      <p>Olá, sou <span>Adriano Volter</span>, tenho 39 anos<br /> Sou <strong>Curso Desenvolvedor FullStack Jr</strong> .
                         </p>
                         <p>
                         </p>
-                        <p>Atualmente estou no modulo 3 do curso.<br /> 
+                        
                         </p>
-                        <p>Meu objetivo é me tornar um <strong>desenvolvedor full stack</strong> <br />Estou estudando as tecnologias mais utilizadas no mercado <br /> Como <strong>ReactJS</strong>, <strong>NodeJS, PostgreSQL</strong>.
+                        <p>Meu objetivo é trabalhar na área como<strong>desenvolvedor full-stack</strong> <br />Usando as tecnologias mais utilizadas no mercado <br /> Como <strong>ReactJS</strong>, <strong>NodeJS, PostgreSQL</strong>.
                         </p> 
                 </div>
                   

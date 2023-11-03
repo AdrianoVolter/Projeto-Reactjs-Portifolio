e831ea577df5c29d13e00afc219790e8f964af78
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-03 15:11:55 -0300

    fix: texto home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ function Home () {
         <div className={styles.apresentacao}>
             <p>Olá,  sou <br />
                 <span>Adriano Volter</span> <br />
-                Dev. Front-End Jr <br />
+                Dev. Full-Stack Jr <br />
                 
             </p>
             <Link 
